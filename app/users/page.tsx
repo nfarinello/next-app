9bcd6f7e5bc7eabb8478c6ec4b85340bd0d26f37
Nicole Farinello <nicole_mattos@live.com>
2024-06-05 00:29:21 +0100

    feat(navbar): added the navbar feature to all the pages
    - added navbar
    
    - changed the layout
    
    - changed global file to fix navbar

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserTable from './UserTable';
+import Link from 'next/link';
 
-const UsersPage = async () => {
+interface Props {
+  searchParams: { sortOrder: string }
+}
+const UsersPage = async ({searchParams: { sortOrder }}: Props) => {
 
   return (
     <div>
       <h1>Users</h1>
-      <UserTable />
+      <Link href="/users/new" className='btn'>New User</Link>
+      <Suspense fallback={<p>Loading...</p>}>
+      <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </div>
   )
 }
